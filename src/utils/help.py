9a78d187fcf5519895043b887766be1dd17efe3d
Prayag Bhatt <prayagbhatt2003@gmail.com>
2024-12-25 15:47:55 +0530

    feat: add help command cog to display available commands

---
+++ src/utils/help.py
@@ -0,0 +1,14 @@
+COMMANDS = [
+    {
+        "command": "/list",
+        "description": "Displays a list of all commands."
+    },
+    {
+        "command": "/explain [query]",
+        "description": "Provides detailed explanation for a query."
+    },
+    {
+        "command": "/event [query]",
+        "description": "Inquires about events, updates, and announcements."
+    },
+]
